b670a54ee36b002ae4cbd152c1073f0fcf130b3f
Kinetic Napier <u61819269@gmail.com>
2026-09-19 19:46:42 +0900

    feat: search list discovery metadata

--- src/article-search.js
+++ src/article-search.js
@@ -1,5 +1,5 @@
 export function matchesArticle(item, word) {
   const query = String(word ?? '').toLowerCase();
-  return [item.id, item.title, item.short, item.summary, ...(item.tags ?? [])]
+  return [item.id, item.title, item.short, ...(item.tags ?? []), ...(item.topics ?? [])]
     .some(value => String(value ?? '').toLowerCase().includes(query));
 }
