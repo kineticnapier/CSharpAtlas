5a87f1a43614c2e764ae848e6b2cb53b9146df48
Kinetic Napier <u61819269@gmail.com>
2026-09-20 07:53:33 +0900

    content: load eighth hourly article batch

--- src/content-loader.js
+++ src/content-loader.js
@@ -15,7 +15,8 @@ export const CONTENT_CATEGORIES = [
   'hourly-batch-004.json',
   'hourly-batch-005.json',
   'hourly-batch-006.json',
-  'hourly-batch-007.json'
+  'hourly-batch-007.json',
+  'hourly-batch-008.json'
 ];
 
 const CONTENT_ID_ALIASES = {
