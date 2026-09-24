caea52750f562b6d410ba4639b3ff86a62f9316d
Kinetic Napier <u61819269@gmail.com>
2026-09-24 20:56:04 +0900

    Register CSharpAtlas article batch 029

--- src/content-loader.js
+++ src/content-loader.js
@@ -36,7 +36,8 @@ export const CONTENT_CATEGORIES = [
   'hourly-batch-025.json',
   'hourly-batch-026.json',
   'hourly-batch-027.json',
-  'hourly-batch-028.json'
+  'hourly-batch-028.json',
+  'hourly-batch-029.json'
 ];
 
 const CONTENT_ID_ALIASES = {
